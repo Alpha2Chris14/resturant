27dcdb520d0ef6a4f8c31291ee9021996ed5c6ce
Christian <onyekachristian14@gmail.com>
2023-02-01 00:37:08 +0100

    added functionality to tabs to be able to navigate

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,55 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:menu/widgets/dashboard_body.dart';
+import 'package:menu/widgets/favourite.dart';
 
-class Dashboard extends StatelessWidget {
+class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final appBar = AppBar(
-      elevation: 0,
-      backgroundColor: Colors.white,
-      foregroundColor: Colors.white,
-      title: const Text(
-        "Menu",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          color: Colors.black87,
-        ),
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  late List<Widget> pages;
+  var currentIndex = 0;
+  void _selectedTab(index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  final appBar = AppBar(
+    elevation: 0,
+    backgroundColor: Colors.white,
+    foregroundColor: Colors.white,
+    title: const Text(
+      "Menu",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        color: Colors.black87,
       ),
-      actions: [
-        Container(
-          width: 40,
-          height: 40,
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.elliptical(1, 1),
-            ),
-            color: Color.fromARGB(224, 6, 184, 172),
+    ),
+    actions: [
+      Container(
+        width: 40,
+        height: 40,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.elliptical(1, 1),
           ),
-          margin: const EdgeInsets.all(8),
-          child: const IconButton(
-            onPressed: null,
-            icon: Icon(
-              size: 16,
-              color: Colors.white,
-              Icons.add_shopping_cart_rounded,
-            ),
+          color: Color.fromARGB(224, 6, 184, 172),
+        ),
+        margin: const EdgeInsets.all(8),
+        child: const IconButton(
+          onPressed: null,
+          icon: Icon(
+            size: 16,
+            color: Colors.white,
+            Icons.add_shopping_cart_rounded,
           ),
-        )
-      ],
-    );
+        ),
+      )
+    ],
+  );
+  @override
+  void initState() {
+    pages = [
+      DashboardBody(appBar.preferredSize.height),
+      Favourite(),
+    ];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
-      body: DashboardBody(appBar.preferredSize.height),
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color.fromARGB(224, 6, 184, 172),
+        onTap: _selectedTab,
+        currentIndex: currentIndex,
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite), label: "Favorite"),
           BottomNavigationBarItem(
               icon: Icon(Icons.calendar_month_outlined), label: "Calender"),
-          BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "user"),
+          // BottomNavigationBarItem(icon: Icon(Icons.account_box), label: "user"),
         ],
       ),
     );
